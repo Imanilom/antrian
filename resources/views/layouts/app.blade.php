ac160ba2715dce95d7792c8cd7c187f2c9a168f8
wiboyanto <kingpredatorzenaku@gmail.com>
2024-11-29 10:57:49 +0700

    last

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,15 +43,25 @@
   <div class="row gy-4">
     <div class="col-lg-4 col-md-6 footer-about">
       <a href="index.html" class="logo d-flex align-items-center">
-        <span class="sitename">Medilab</span>
+        {{-- <span class="sitename">Medilab</span>
       </a>
       <div class="footer-contact pt-3">
         <p>A108 Adam Street</p>
         <p>New York, NY 535022</p>
         <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
         <p><strong>Email:</strong> <span>info@example.com</span></p>
-      </div>
-      <div class="social-links d-flex mt-4">
+      </div> --}}
+
+      <span class="sitename">UPT Klinik Bumi Medika ITB</span>
+    </a>
+     <div class="footer-contact pt-3">
+      <p>Jl. Gelap Nyawang No.2, Lb. Siliwangi, Kecamatan Coblong</p>
+      <p>Kota Bandung, Jawa Barat 40132</p>
+      <p class="mt-3"><strong>Phone:</strong> <span>081294488766 / 022 2500082</span></p>
+      <p><strong>Email:</strong> <span>yankes@itb.ac.id</span></p>
+    </div>
+
+      {{-- <div class="social-links d-flex mt-4">
         <a href=""><i class="bi bi-twitter-x"></i></a>
         <a href=""><i class="bi bi-facebook"></i></a>
         <a href=""><i class="bi bi-instagram"></i></a>
@@ -104,10 +114,10 @@
     </div>
 
   </div>
-</div>
+</div> --}}
 
 <div class="container copyright text-center mt-4">
-  <p>© <span>Copyright</span> <strong class="px-1 sitename">Medilab</strong> <span>All Rights Reserved</span></p>
+  <p>© <span>Copyright</span> <strong class="px-1 sitename">UPT Klinik Bumi Medika ITB</strong> <span>All Rights Reserved</span></p>
   <div class="credits">
     <!-- All the links in the footer should remain intact. -->
     <!-- You can delete the links only if you've purchased the pro version. -->
